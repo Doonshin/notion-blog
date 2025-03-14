2dc096970bf5b396183a6e8b85531e6d8b88af77
Shinji Kato <katooo.1031@gmail.com>
2025-03-13 20:04:52 -0500

    Update

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.notion.so",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ site.config.tsx
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  title: "Notion Next Blog",
+  twitterUrl: "",  
+  instagramUrl: "https://www.instagram.com/",  
+  githubUrl: "https://github.com/Doonshin",  
+}
+
+export default siteConfig;
